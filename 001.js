a81b763dfa61d0f5b8b485508877ede9911c834f
Firmansyah Rozy <rozy.firmansyah@gmail.com>
2022-05-20 16:32:10 +0700

    commit file 001.js

---
+++ 001.js
@@ -0,0 +1,11 @@
+// buatlah sebuah fungsi bernama jumlah, yang menerima 2 buah parameter integer, kemudian men-return hasil penjumlahan dari parameter tersebut
+
+function jumlah(angka1, angka2) {
+    return 0;
+}
+
+jumlah(1, 2); // 3
+jumlah(1, 4); // 5
+jumlah(-1, -17); // -18
+jumlah(0, 0); // 0
+jumlah(2, 4); // 6
